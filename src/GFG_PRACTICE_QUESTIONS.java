372be862ecee0e0902098552ea9a6e85ca91c447
Tejas Rajendra Bhoite <130844340+TejasBhoite0707@users.noreply.github.com>
2025-01-20 23:16:45 +0530

    Update GFG_PRACTICE_QUESTIONS

--- src/GFG_PRACTICE_QUESTIONS.java
+++ src/GFG_PRACTICE_QUESTIONS.java
@@ -33,3 +33,22 @@ class Solution {
         return head;
     }
 }
+
+class Solution {
+    public Node insertInMiddle(Node head, int x) {
+        // Code here
+        Node slow=head;
+        Node fast=head;
+        Node newNode=new Node(x);
+         if (head == null) {
+            return newNode;
+        }
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        newNode.next=slow.next;
+        slow.next=newNode;
+        return head;
+    }
+}
