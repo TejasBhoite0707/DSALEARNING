75d87b3523863f7904911b24772e1ff7339fb444
Tejas Rajendra Bhoite <130844340+TejasBhoite0707@users.noreply.github.com>
2025-01-22 09:24:05 +0530

    Update GFG_PRACTICE_QUESTIONS

--- src/GFG_PRACTICE_QUESTIONS.java
+++ src/GFG_PRACTICE_QUESTIONS.java
@@ -148,3 +148,24 @@ class Solution {
         return temp.data;
     }
 }
+//Alternative Approach
+class Solution {
+
+    // Function to find the data of kth node from
+    // the end of a linked list.
+    int getKthFromLast(Node head, int k) {
+        // Your code here
+        Node first=head;
+        Node second=head;
+        if(head==null) return -1;
+        for(int i=0;i<k;i++){
+            if(first==null) return -1;
+            first=first.next;
+        }
+        while(first!=null){
+            first=first.next;
+            second=second.next;
+        }
+        return second.data;
+    }
+}
