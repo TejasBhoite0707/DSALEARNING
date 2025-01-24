ac4d202f3d97219af990296d974ef40696ff4e73
Tejas Rajendra Bhoite <130844340+TejasBhoite0707@users.noreply.github.com>
2025-01-24 12:31:24 +0530

    Update GFG_PRACTICE_QUESTIONS

--- src/GFG_PRACTICE_QUESTIONS.java
+++ src/GFG_PRACTICE_QUESTIONS.java
@@ -263,3 +263,34 @@ class Solution {
         return false;
     }
 }
+
+
+// Given a linked list where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to the head side, 2s at the end of the linked list, and 1s in the middle of 0s and 2s.
+
+// Examples:
+
+// Input: LinkedList: 1->2->2->1->2->0->2->2
+// Output: 0->1->1->2->2->2->2->2
+// Explanation: All the 0s are segregated to the left end of the linked list, 2s to the right end of the list, and 1s in between.
+class Solution {
+    // Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head) {
+        // add your code here
+        Node Temp=head;
+        ArrayList<Integer> arr=new ArrayList<>();
+        while(Temp!=null){
+            arr.add(Temp.data);
+            Temp=Temp.next;
+        }
+        Collections.sort(arr);
+    
+    Temp=head;
+    int position_OR_index=0;
+    while(Temp!=null){
+        Temp.data=arr.get(position_OR_index);
+        position_OR_index++;
+        Temp=Temp.next;
+    }
+       return head;
+    }
+}
