ea7c5ea9e5ff497a1a3d4148400fbc7bf2255a7c
Tejas Rajendra Bhoite <130844340+TejasBhoite0707@users.noreply.github.com>
2025-01-29 09:49:58 +0530

    Update OtherQuestions

--- src/OtherQuestions.java
+++ src/OtherQuestions.java
@@ -1,7 +1,7 @@
-Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
-Examples:
-Input: b = 3.00000, e = 5
-Output: 243.00000
+// Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
+// Examples:
+// Input: b = 3.00000, e = 5
+// Output: 243.00000
 
 class Solution {
     public double power(double b, int e) {
