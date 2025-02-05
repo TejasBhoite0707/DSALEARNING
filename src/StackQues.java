418b4a9f877d882ecb1520d64ebd90bcd97f4d4c
Tejas Rajendra Bhoite <130844340+TejasBhoite0707@users.noreply.github.com>
2025-02-05 09:37:09 +0530

    Implement stack using array

--- src/StackQues.java
+++ src/StackQues.java
@@ -116,3 +116,25 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+//Implement stack using array
+class MyStack {
+    private int[] arr;
+    private int top;
+
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    }
+
+    public void push(int x) {
+        // Your Code
+        arr[++top]=x;
+    }
+
+    public int pop() {
+        // Your Code
+        if(top==-1) return -1;
+        return arr[top--];
+    }
+}
