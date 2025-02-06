a54d0e51ec1939d38b21958e6d77f91af8905fd9
Tejas Rajendra Bhoite <130844340+TejasBhoite0707@users.noreply.github.com>
2025-02-06 10:43:38 +0530

    stack using linked list

--- src/StackQues.java
+++ src/StackQues.java
@@ -138,3 +138,31 @@ class MyStack {
         return arr[top--];
     }
 }
+
+//stack using linkedlist
+class MyStack {
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }
+    StackNode top;
+    // Function to push an integer into the stack.
+    void push(int a) {
+        // Add your code here
+        StackNode newNode=new StackNode(a);
+        newNode.next=top;
+        top=newNode;
+    }
+    // Function to remove an item from top of the stack.
+    int pop() {
+        // Add your code here
+        if (top == null) return -1;
+        int topele=top.data;
+        top=top.next;
+        return topele;
+    }
+}
