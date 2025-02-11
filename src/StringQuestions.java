ad44825b5c6916ade77dc630b2297f8d8b521332
Tejas Rajendra Bhoite <130844340+TejasBhoite0707@users.noreply.github.com>
2025-02-11 12:10:26 +0530

    Delete Array Elements

--- src/StringQuestions.java
+++ src/StringQuestions.java
@@ -114,3 +114,29 @@ class Solution {
         return stackans.size();
     }
 }
+
+// Given an array arr[] and a number k. The task is to delete k elements that are smaller than the next element (i.e., we delete arr[i] if arr[i] < arr[i+1]) or become smaller than the next because the next element is deleted.
+// Examples:
+// Input: arr[] = [20, 10, 25, 30, 40], k = 2
+// Output: [25, 30, 40]
+// Explanation: First we delete 10 because it follows arr[i] < arr[i+1]. Then we delete 20 because 25 is moved next to it and it also starts following the condition.
+class Solution {
+    // Function for finding maximum and value pair
+    public static ArrayList<Integer> deleteElement(int arr[], int k) {
+        // Complete the function
+        Stack<Integer>ArrEleStack=new Stack<>();
+        for(int arrEle:arr){
+            if(ArrEleStack.isEmpty() && k==0){
+                ArrEleStack.push(arrEle);
+            }
+            else{
+                while(!ArrEleStack.isEmpty() && k>0 && ArrEleStack.peek()<arrEle){
+                    ArrEleStack.pop();
+                    k--;
+                }
+                ArrEleStack.push(arrEle);
+            }
+        }
+        return new ArrayList<>(ArrEleStack);
+    }
+}
