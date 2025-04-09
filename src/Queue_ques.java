941dd0a914d889e77675575f8a9adb614cfd3393
Tejas Rajendra Bhoite <130844340+TejasBhoite0707@users.noreply.github.com>
2025-04-09 20:06:01 +0530

    Generate Binary Numbers

--- src/Queue_ques.java
+++ src/Queue_ques.java
@@ -17,3 +17,23 @@ public static int front_dq(ArrayDeque<Integer> dq) {
 public static void pf(ArrayDeque<Integer> dq, int x) {
      dq.offerFirst(x);
 }
+
+//Generate Binary Numbers
+class solve {
+    // Function to generate binary numbers from 1 to N using a queue.
+    static ArrayList<String> generate(int n) {
+        // Your code here
+        ArrayList<String> result=new ArrayList<>();
+        Queue<String>q=new LinkedList<>();
+        q.offer("1");
+        
+        for(int i=0;i<n;i++){
+            result.add(q.poll());
+            String n1=result.get(i)+"0";
+            String n2=result.get(i)+"1";
+            q.offer(n1);
+            q.offer(n2);
+        }
+        return result;
+    }
+}
